7dda87e583fda91e616b3fa34d40bdab2a2e7e30
Derrick Furtado <derrick.furtado10@gmail.com>
2024-02-29 11:28:19 -0700

    ready to deploy

---
+++ public/server.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '6df54f27d09443c28927809145fdeb6c',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Rollbar connection is live')
+
+app.use(express.json())
+
+const students = ['Jimmy', 'Timothy', 'Jimothy']
+
+app.get('/', (req, res) => {
+    rollbar.info("Someone got the student list")
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('/api/students', (req, res) => {
+    res.status(200).send(students)
+})
+
+app.post('/api/students', (req, res) => {
+   let {name} = req.body
+
+   const index = students.findIndex(student => {
+       return student === name
+   })
+
+   try {
+       if (index === -1 && name !== '') {
+           students.push(name)
+           res.status(200).send(students)
+       } else if (name === ''){
+        //    rollbar.critical("User attempted to enter a ghost")
+           res.status(400).send('You must enter a name.')
+       } else {
+        //    rollbar.warning("User tried to duplicate data")
+           res.status(400).send('That student already exists.')
+       }
+   } catch (err) {
+       console.log(err)
+   }
+})
+
+app.delete('/api/students/:index', (req, res) => {
+    const targetIndex = +req.params.index
+    
+    students.splice(targetIndex, 1)
+    res.status(200).send(students)
+})
+
+const port = process.env.PORT || 5050
+
+app.listen(port, () => console.log(`Server listening on ${port}`))
